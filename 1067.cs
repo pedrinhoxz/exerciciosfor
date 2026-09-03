c122329123771846fa2d47dabf083204f952feba
pedrinhoxz <pedromine91@gmail.com>
2026-09-03 17:19:36 -0300

    1067

---
+++ 1067.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= x; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
